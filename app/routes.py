c54af3068fdc20a6ded638da6b6db10e26fea962
Syper11 <brettrickheim@gmail.com>
2023-01-27 11:27:19 -0600

    second

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,5 @@
 from app import app
-from flask import render_template, request, flash, url_for, redirect
-from .forms import UserCreationForm
+from flask import render_template, flash, url_for, redirect
 from .models import Product
 from flask_login import current_user, login_required
 
