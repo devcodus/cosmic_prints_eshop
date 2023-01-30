2452d23ba8d22d6e5d9fe87bf22d7bab83ea563e
Syper11 <brettrickheim@gmail.com>
2023-01-29 21:15:41 -0600

    html/css styling 1

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -46,7 +46,7 @@ def login():
                 #if user ecxists, check if passwords match
                 if user.password == password:
                     login_user(user)
-                    return redirect(url_for('homepage'))
+                    return redirect(url_for('homePage'))
                 else:
                     print('wrong password')
 
